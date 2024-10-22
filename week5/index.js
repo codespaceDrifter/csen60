2ff0b6f1003b6ba87c3ed9a04f69e113d98819ea
codeCraftCorpo <codeCraftCorpo@gmail.com>
2024-10-21 19:06:02 -0700

    javascript

---
+++ week5/index.js
@@ -0,0 +1,9 @@
+console.log("hello world");
+alert("Hello world dialog");
+
+document.write('<p>Welcome to CSEN 60!</p>');
+console.log("This should be a");
+
+function add(a, b) {
+    return a + b;
+}
