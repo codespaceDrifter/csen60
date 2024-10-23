d675e7f18cae6012eac038b00dfb1cb8508f9c77
codeCraftCorpo <codeCraftCorpo@gmail.com>
2024-10-23 11:34:33 -0700

    javascript operators

--- week5/index.js
+++ week5/index.js
@@ -1,9 +1,39 @@
 console.log("hello world");
-alert("Hello world dialog");
-
-document.write('<p>Welcome to CSEN 60!</p>');
-console.log("This should be a");
 
 function add(a, b) {
     return a + b;
 }
+
+let x = 100;
+if (true) {
+    x = 200; // Same variable!
+}
+console.log(x)
+
+let y
+y = null
+let z
+console.log(y, z)
+
+let greeting = "Hello, world!";
+let response = `The time is ${new Date()}`;
+console.log (response);
+
+const age = 16
+
+if (age > 18){
+    console.log("of age")
+} else {
+    console.log(`wait ${18-age} years`)
+}
+
+let person = {
+  name: "John",
+  age: 30,
+  isStudent: false
+};
+
+console.log(person.age)
+console.log(typeof(person))
+
+console.log (false == 0)
